6644fbd06bce6d81744fd3c2e987c560f0a81034
amitjaiswar1502 <amitjaiswar1502@gmail.com>
2025-03-12 01:57:42 +0530

    homepage and theming for app done

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute z-0 w-full" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 100 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
+    </View>
+  );
+}
