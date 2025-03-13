e05828aa14ec9b78321d1694ca48237d1f4a373d
amitjaiswar1502 <amitjaiswar1502@gmail.com>
2025-03-13 17:09:15 +0530

    Movie card created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,28 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+import SearchBar from "../components/SearchBar";
+import { useRouter } from "expo-router";
+import useFetch from "../services/useFetch";
+import { fetchMovies } from "../services/api";
+import MovieCard from "../components/MovieCard";
 
 export default function Index() {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute z-0 w-full" />
@@ -12,6 +32,43 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 100 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#ab8bff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <Text>Error:{moviesError.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => (
+                  <MovieCard {...item}/>
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 10,
+                  marginBottom: 10,
+                }}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const search = () => {
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 items-center justify-center'>
+      <Text>Hello sexy</Text>
     </View>
   )
 }
